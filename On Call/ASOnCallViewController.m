ea617dc908c17d2216263d6693afc781642f75b4
RobbieClarken <robbie.clarken@gmail.com>
2013-12-03 23:48:55 +1100

    Request address book access

--- On Call/ASOnCallViewController.m
+++ On Call/ASOnCallViewController.m
@@ -8,10 +8,13 @@
 
 #import "ASOnCallViewController.h"
 #import "ASPerson.h"
+#import <AddressBook/AddressBook.h>
 
 @interface ASOnCallViewController ()
 
+@property (assign, nonatomic) ABAddressBookRef addressBook;
 @property (strong, nonatomic) NSArray *peopleOnCall;
+@property (nonatomic) BOOL addressBookAccessGranted;
 
 @end
 
@@ -19,7 +22,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     self.tableView.contentInset = UIEdgeInsetsMake(20.0f, 0.0f, 0.0f, 0.0f);
     
     self.peopleOnCall = @[
@@ -27,6 +29,36 @@
         [ASPerson personWithName:@"John Brown" inGroup:@"Mechanical"],
         [ASPerson personWithName:@"Jane Black" inGroup:@"Controls"]
     ];
+    
+    self.addressBook =  ABAddressBookCreateWithOptions(NULL, NULL);
+    [self checkAddressBookAccess];
+}
+
+#pragma mark - Address book access
+
+-(void)checkAddressBookAccess {
+    switch (ABAddressBookGetAuthorizationStatus()) {
+        case kABAuthorizationStatusAuthorized:
+            // All good
+            self.addressBookAccessGranted = YES;
+            break;
+        case kABAuthorizationStatusNotDetermined:
+            // User has not specified
+            [self requestAddressBookAccess];
+            break;
+        case kABAuthorizationStatusDenied:
+        case kABAuthorizationStatusRestricted:
+            self.addressBookAccessGranted = NO;
+            break;
+    }
+}
+
+- (void)requestAddressBookAccess {
+    ASOnCallViewController * __weak weakSelf = self;
+    
+    ABAddressBookRequestAccessWithCompletion(weakSelf.addressBook, ^(bool granted, CFErrorRef error) {
+        weakSelf.addressBookAccessGranted = granted;
+    });
 }
 
 #pragma mark - Table view data source
